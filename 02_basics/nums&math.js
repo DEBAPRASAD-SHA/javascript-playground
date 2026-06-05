35effd04a4ca7301139eba66cd309b018686eaaa
DEBAPRASAD SHA <debaprasadsha98@gmail.com>
2026-06-05 23:48:24 +0530

    .

---
+++ 02_basics/nums&math.js
@@ -0,0 +1,35 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 528.3899
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// maths
+
+//  console.log(Math);
+// console.log(Math.abs(-4));//abs used for convert the -ve value to +ve value & +ve stays with +ve
+// console.log(Math.round(4.6)); //round is use to round up the value according to maths rules
+// console.log(Math.ceil(4.2)); //ceil always choose top value for round up
+// console.log(Math.floor(4.8)); //floor always choose bottom value for round up
+// console.log(Math.min(4, 3, 5, 7)); //min used for choose minimum value
+// console.log(Math.max(4, 3, 5, 7)); //floor used for choose maximum value
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
