ab350384472ec1bc806afeba386ffe517349a482
DEBAPRASAD SHA <debaprasadsha98@gmail.com>
2026-06-23 23:35:59 +0530

    loops discussion

--- 05_iterations/six.js
+++ 05_iterations/six.js
@@ -2,28 +2,27 @@
 
 
 // const values = coding.forEach( (item) => {
-//     //console.log(item);
-//     return item
+//   // console.log(item);
+//   return item
 // } )
 
 // console.log(values);
 
-const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const myNums = [1,2,3,4,5,6,7,8,9]
 
 // const newNums = myNums.filter( (num) => {
 //     return num > 4
-// } )
+// // }  )
 
 // const newNums = []
 
 // myNums.forEach( (num) => {
-//     if (num > 4) {
+//     if (num > 4){
 //         newNums.push(num)
 //     }
 // } )
 
 // console.log(newNums);
-
 
 const books = [
     { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
@@ -39,7 +38,5 @@ const books = [
 
   let userBooks = books.filter( (bk) => bk.genre === 'History')
 
-  userBooks = books.filter( (bk) => { 
-    return bk.publish >= 1995 && bk.genre === "History"
-})
+   userBooks = books.filter( (bk => {return bk.publish >= 1995 && bk.genre ==="History"}) )
   console.log(userBooks);
