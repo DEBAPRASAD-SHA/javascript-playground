d96db23d24cf02b9aad6e693aea535402436f293
DEBAPRASAD SHA <debaprasadsha98@gmail.com>
2026-06-24 23:00:08 +0530

    loops discussion

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -2,9 +2,9 @@ const myNumers = [1,2,3,4,5,6,7,8,9]
 
 // const newNums = myNumers.map( (num) => {return num + 10} )
 
-const newNums =  myNumbers
-.map((num) => num*10)
-.map( () => )
-.map ( (num) =>)
+const newNums =  myNumers
+.map((num) => num * 10)
+.map( (num) => num + 1 )
+.map ( (num) => num >= 40)
 
 console.log(newNums);
